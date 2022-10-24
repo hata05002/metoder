fd5e8d7911e758c556b6515c13b95f9fa59fb42d
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-10-24 09:39:46 +0200

    Add files via upload
    uppgift 2 medelvärde funktion

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 class program{
    public static void Main(){
-        double radie=2.4;
-	Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
-	Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
-}
-// pi-värde är bestämt för 3.14
-static double CirkelOmkrets(double r){
-    double omkrets= 2*3.14*r;
-    return omkrets;
-}
 
-static double CirkelArea(double r){
-    double area= r*r*3.14;
-    return area;
-}
+Console.WriteLine("Skriv tre olika tal:");
 
-    }
+double t1= Convert.ToDouble(Console.ReadLine());
+double t2= Convert.ToDouble(Console.ReadLine());
+double t3= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("medelvärde av talen är:"+ Medel(t1,t2,t3));
+
+}
+ static double Medel(double a, double s, double d){
+    double medel= a+s+d;
+    double medelo= medel/3;
+    return medelo;
+    
+}
+     }
