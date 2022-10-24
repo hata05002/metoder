fcfe98378074221f14f62bb1c97a74770aabac66
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-10-24 09:41:49 +0200

    Add files via upload

---
+++ program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class program{
+   public static void Main(){
+        double radie=2.4;
+	Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
+	Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
+}
+// pi-värde är bestämt för 3.14
+static double CirkelOmkrets(double r){
+    double omkrets= 2*3.14*r;
+    return omkrets;
+}
+
+static double CirkelArea(double r){
+    double area= r*r*3.14;
+    return area;
+}
+
+    }
